f26e102eb333e428d339badde365f8003b3831b0
carlosgalvesisep <1211604@isep.ipp.pt>
2022-12-17 12:08:53 +0000

    Nome da aplicação corrigido para "ProjectReact"

--- ios/ProjectReactTests/ProjectReactTests.m
+++ ios/ProjectReactTests/ProjectReactTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ProjectReadTests : XCTestCase
+@interface ProjectReactTests : XCTestCase
 
 @end
 
-@implementation ProjectReadTests
+@implementation ProjectReactTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
